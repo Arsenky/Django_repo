234b405e70964382c1a370929d493b7a28f09dfa
arsenky <arsenky@yandex.ru>
2022-11-02 18:31:42 +0300

    lesson_2_complited

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
+
+class CoursesPageView(TemplateView):
+    template_name = "mainapp/courses_list.html"
+
+class NewsPageView(TemplateView):
+    template_name = "mainapp/news.html"
+
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
+
+class DocSitePageView(TemplateView):
+    template_name = "mainapp/doc_site.html"
+
+class LoginPageView(TemplateView):
+    template_name = "mainapp/login.html"
 # Create your views here.
