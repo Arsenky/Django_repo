9f53d99a3b8f548d3931007684d03aabcb745e89
arsenky <arsenky@yandex.ru>
2022-12-11 14:19:47 +0300

    lesson_8 Tests

--- mainapp/views.py
+++ mainapp/views.py
@@ -22,30 +22,6 @@ logger = logging.getLogger(__name__)
 class MainPageView(TemplateView):
     template_name = "mainapp/index.html"
 
-# class CoursesPageView(TemplateView):
-#     template_name = "mainapp/courses_list.html"
-
-# class NewsPageView(TemplateView):
-#     template_name = "mainapp/news.html"
-
-#     def get_context_data(self, **kwargs): 
-#         # Get all previous data 
-#         context=super().get_context_data(**kwargs) 
-#         # Create your own data 
-#         context["news_title"] = "Громкий новостной заголовок"
-#         context[ "news_preview" ] = "Предварительное описание, которое заинтересует каждого" 
-#         context["range"] = range(5)
-#         context["datetime_obj"] =datetime.now()
-#         context["news_qs"] = mainapp_models.News.objects.all()[:10]
-#         return context
-
-# class NewsWithPaginatorView(NewsPageView):
-    
-#     def get_context_data(self,page, **kwargs):
-#         context=super().get_context_data(page = page,**kwargs) 
-#         context["page_num"] = page
-#         return context
-
 class ContactsPageView(TemplateView):
     template_name = "mainapp/contacts.html"
 
@@ -134,46 +110,39 @@ class CoursesListView(TemplateView):
         context["objects"] = mainapp_models.Courses.objects.all()[:7] 
         return context 
         
-# class CoursesDetailView(TemplateView): 
-#     template_name = "mainapp/courses_detail.html" 
-    
-#     def get_context_data(self, pk=None, **kwargs): 
-#         context = super(CoursesDetailView, self).get_context_data(**kwargs) 
-#         context["course_object"] = get_object_or_404( mainapp_models.Courses, pk=pk ) 
-#         context["lessons"] = mainapp_models.Lesson.objects.filter( course=context["course_object"] ) 
-#         context["teachers"] = mainapp_models.CourseTeachers.objects.filter( course=context["course_object"]) 
-#         return context
-
 class CoursesDetailView(TemplateView):
     template_name = "mainapp/courses_detail.html"
 
     def get_context_data(self, pk=None, **kwargs):
         logger.debug("Yet another log message")
         context = super(CoursesDetailView, self).get_context_data(**kwargs)
-        context["course_object"] = get_object_or_404(
-        mainapp_models.Courses, pk=pk
-        )
-        context["lessons"] = mainapp_models.Lesson.objects.filter(
-        course=context["course_object"]
-        )
-        context["teachers"] = mainapp_models.CourseTeachers.objects.filter(
-        course=context["course_object"]
-        )
+        context["course_object"] = get_object_or_404(mainapp_models.Courses, pk=pk)
+        context["lessons"] = mainapp_models.Lesson.objects.filter(course=context["course_object"])
+        context["teachers"] = mainapp_models.CourseTeachers.objects.filter(course=context["course_object"])
         if not self.request.user.is_anonymous:
             if not mainapp_models.CourseFeedback.objects.filter(
                 course=context["course_object"], user=self.request.user
-                ).count():
+            ).count():
                 context["feedback_form"] = mainapp_forms.CourseFeedbackForm(
-                course=context["course_object"], user=self.request.user
+                    course=context["course_object"], user=self.request.user
                 )
-                cached_feedback = cache.get(f"feedback_list_{pk}")
-        if not cached_feedback:
-            context["feedback_list"] = mainapp_models.CourseFeedback.objects.filter(
-                course=context["course_object"]).order_by(
-                    "-created", "-rating"
-                )[:5].select_related()
 
-            cache.set(f"feedback_list_{pk}", context["feedback_list"], timeout=300) # 5 minutes
+        cached_feedback = cache.get(f"feedback_list_{pk}")
+        if not cached_feedback:
+            context["feedback_list"] = (
+                mainapp_models.CourseFeedback.objects.filter(course=context["course_object"])
+                .order_by("-created", "-rating")[:5]
+                .select_related()
+            )
+            cache.set(f"feedback_list_{pk}", context["feedback_list"], timeout=300)  # 5 minutes
+
+            # Archive object for tests --->
+            import pickle
+
+            with open(f"mainapp/fixtures/005_feedback_list_{pk}.bin", "wb") as outf:
+                pickle.dump(context["feedback_list"], outf)
+            # <--- Archive object for tests
+
         else:
             context["feedback_list"] = cached_feedback
 
